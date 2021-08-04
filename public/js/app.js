cec132f343a00bbc85fb9b8afc6304cbd50d88e2
Justin Santos <justin.santos1997@gmail.com>
2021-08-04 08:50:02 +0800

    Set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -23,8 +23,6 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-messageOne.textContent = "From JS";
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
@@ -32,16 +30,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
